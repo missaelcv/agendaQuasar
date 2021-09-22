32722b21e666f689e5c10663d19e776eb1541522
missaelcv <missaelcv0220@gmail.com>
2021-09-22 09:42:28 -0400

    layout drawer sin clase

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,27 +11,26 @@
         v-model="drawer"
         show-if-above
         :width="200"
-        :breakpoint="400"
-      >
+        :breakpoint="400">
         <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
           <q-list padding>
-            <q-item clickable v-ripple>
+            <q-item clickable v-ripple to="/" >
               <q-item-section avatar>
                 <q-icon name="inbox" />
               </q-item-section>
 
               <q-item-section>
-                Inbox
+              Inicio
               </q-item-section>
             </q-item>
 
-            <q-item active clickable v-ripple>
+            <q-item active clickable v-ripple to="/registrar">
               <q-item-section avatar>
                 <q-icon name="star" />
               </q-item-section>
 
               <q-item-section>
-                Star
+               Nuevo Registro
               </q-item-section>
             </q-item>
 

0497deb969004e80562bf799a7d5a74b30be9243
missaelcv <missaelcv0220@gmail.com>
2021-09-22 10:59:17 -0400

    creando iput de formularios

--- src/pages/Registrar.vue
+++ src/pages/Registrar.vue
@@ -1,6 +1,96 @@
 <template>
   <q-page padding>
-    <h1>About</h1>
+
+ <h4 class="col-12 col-sm-6 text-center "> Registrar Nuevo Contacto</h4>
+
+
+    <h5 class="text-left">Formulario</h5>
+
+<div class=q-gutter-md >
+    <q-form 
+      @submit="onSubmit"
+      @reset="onReset"
+      class="q-gutter-md"
+      label= "Formulario"> 
+
+ <div class="row q-col-gutter-md">
+      <q-input class= "col-12 col-sm-6"
+        filled
+        v-model="name"
+        label="Escriba el Nombre*"
+        hint="Name"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'No Puede estar Vacio']"/>
+        
+
+        <q-input class= "col-12 col-sm-6"
+        filled
+        v-model="apellido"
+        label="Escriba su Apellido*"
+        hint="Surname"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'No Puede estar Vacio']"/>
+        
+     
+        <q-input class= "col-12 col-sm-6"
+        filled
+        v-model="correo"
+        label="Escriba su Correo Electronico*"
+        hint="Email"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'No Puede estar Vacio']"/>
+        
+
+        <q-input class= "col-12 col-sm-6"
+        filled
+        v-model="direccion"
+        label="Escriba su Direccion*"
+        hint="Address"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'No Puede estar Vacio']"/>
+       
+
+        <q-input class= "col-12 col-sm-6"
+        filled
+        v-model="cuidad"
+        label="Escriba su Cuidad*"
+        hint="City"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'No Puede estar Vacio']"/>
+         
+
+
+      <q-input class= "col-12 col-sm-6"
+        filled
+        type="number"
+        v-model="numbe"
+        label="Telefono Movil *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val !== '' || 'No puede estar vacio',
+          val => val > 0 && val < 100 || 'Please type a real age']" />
+
+        <q-input class= "col-12 col-sm-6"
+        filled
+        type="number"
+        v-model="numbe2"
+        label="Telefono Residencial *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val !== '' || 'No puede estar vacio',
+          val => val > 0 && val < 100 || 'Please type a real age']" />
+           </div>
+
+       
+      <q-toggle v-model="accept" label="Desea aceptar los términos" />
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary"/>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form>
+
+  </div> 
   </q-page>
 </template>
 
